19b1ee55b90c846b6f74f908268886d674dd02e5
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-05-27 22:05:16 +0300

    Completed Assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+
+    # breed = "Mutt"
+
+    def __init__(self, dog_name, breed = "Mutt"):
+        self.name = dog_name
+        self.breed = breed
+
+dog = Dog(dog_name="Bosco", breed="German")
+# bosco = Dog(dog_name="Bosco", breed="breed")
+print(dog.breed)
